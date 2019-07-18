f0641b41074e7618c70b87c4a466d4728307044b
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2019-07-18 05:36:01 +0000

    version 0.0.5

---
+++ R/ordfrey.R
@@ -0,0 +1,15 @@
+#' Frey Faces Isomap
+#'
+#' 
+#' @details 
+#' Dissimilarity object of class 'isomap' for Frey Faces data, created with:
+#' 
+#' \code{isomap(vegdist(t(frey), method="euclidean"),k = 12, ndim=6, fragmentedOK = TRUE)}
+#' 
+#' Introduced simply to cache the results of this step so as to speed up demos.
+#' 
+#' @name ordfrey
+#' @docType data
+#' @format Object of class 'isomap'.
+#' @keywords data
+NULL
